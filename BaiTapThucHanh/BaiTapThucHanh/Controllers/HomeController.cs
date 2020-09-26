c69b41a0b4799d1df7a7be98e5d4a14d6248f26e
thanh1998 <thanhstone98@gmail.com>
2020-09-26 15:55:29 +0700

    Update project

---
+++ BaiTapThucHanh/BaiTapThucHanh/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using BaiTapThucHanh.Models;
+
+namespace BaiTapThucHanh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> data = new List<Product>();
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/Data/generated.json")))
+            {
+                data = JsonConvert.DeserializeObject<List<Product>>(sr.ReadToEnd());
+            }
+                return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BaiTapThucHanh/BaiTapThucHanh/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+
+namespace BaiTapThucHanh.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public long price { get; set; }
+        public string image { get; set; }
+    }
+}
